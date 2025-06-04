af004dfd600c1259c6e7b718a88508bd25d6e5ee
Sophia Rose Nunez <srnunez@meta.com>
2025-06-03 17:45:31 -0700

    [Milestone 1 & 2] Completed modal display and transitions with hardcoded webpage

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,21 @@
+const modal = document.getElementById("playlist-modal");
 
+const playlistCards = document.querySelectorAll('.playlist');
+
+const span = document.getElementsByClassName('close')[0];
+
+playlistCards.forEach(playlistCard => {
+    playlistCard.addEventListener('click', function() {
+        modal.style.display = "block";
+    });
+});
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
