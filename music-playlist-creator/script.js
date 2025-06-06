cb59fee9f397f89ec7c0a1a088121c91af735211
Sophia Rose Nunez <srnunez@meta.com>
2025-06-05 23:43:36 -0700

    [Add/edit/delete] Complete delete functionality, add/edit in progress.

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -2,6 +2,11 @@
 const playlistCards = document.getElementById('playlist-cards');
 const modalContent = document.getElementById('modal-content');
 
+document.addEventListener("DOMContentLoaded", function () {
+  loadPageName();
+
+})
+
 async function fetchData() {
        try {
            const response = await fetch('./data/data.json');
@@ -15,23 +20,75 @@ async function fetchData() {
        }
    }
 
-async function loadPlaylists() {
-    // home page
-    const playlists = await fetchData();
+function loadPageName() {
+    const pageName = document.getElementById('page-name');
+    const path = window.location.pathname;
+    const fileName = path.substring(path.lastIndexOf('/') + 1).toLowerCase();
+
+    let text = '';
+
+    switch (fileName) {
+        case 'index.html':
+        text = 'All Playlists';
+        break;
+        case 'featured.html':
+        text = 'Featured Playlist';
+        break;
+        default:
+        text = 'Page not recognized.';
+    }
+    console.log(text);
+    pageName.textContent = text;
+}
+
+async function loadHome() {
+    loadPlaylists(await fetchData());
+
+    // home page, make add button listender first
+    const addBtn = document.getElementById('add-btn');
+    addBtn.addEventListener('click', () => {
+        openAddModal();
+    })
+
+    // form listener since modal is open
+    const form = document.getElementById('add-form');
+
+}
+
+async function loadPlaylists(playlists) {
+    playlistCards.innerHTML = `
+        <article id="add-container" class="playlist">
+            <i id="add-btn" class="fa fa-plus" aria-hidden="true"></i>
+        </article>
+    `;
+    // now load playlists
     if (playlists.length === 0) {
         const noLists = document.createElement('h3');
         noLists.textContent = 'No playlists added';
     } else {
         playlists.forEach(playlist => {
             let playlistCard = document.createElement('article');
-            const playlistCover = document.createElement('img')
-            playlistCover.className = "playlist-cover"
-            playlistCover.src = playlist.playlist_art
-
+            
             playlistCard.className = 'playlist';
-            playlistCard.innerHTML = 
+            playlistCard.innerHTML = createPlaylistCard(playlist);
+            playlistCards.appendChild(playlistCard);
+            
+            // add listener for modal to each playlist
+            playlistCard.addEventListener('click', () => {
+                openModal(playlist);
+            })
+        })
+    }
+}
+
+function createPlaylistCard(playlist) {
+    // const playlistCover = document.createElement('img')
+    // playlistCover.className = "playlist-cover"
+    // playlistCover.src = playlist.playlist_art
+
+    playlistHTML = 
             `
-            <div class="img-wrapper"><img class="playlist-cover" src="${playlist.playlist_art}"><i class="fa-solid fa-pencil playlist-icon" onclick="editPlaylist(event)"></i><i class="fa fa-trash playlist-icon" aria-hidden="true" onclick="deletePlaylist(event)"></i></img></div>
+            <div class="img-wrapper"><img class="playlist-cover" src="${playlist.playlist_art}"><i class="fa-solid fa-pencil playlist-icon" onclick="editPlaylist(event)" id="${playlist.playlistID}"></i><i class="fa fa-trash playlist-icon" aria-hidden="true" onclick="deletePlaylist(event)" id="${playlist.playlistID}"></i></img></div>
             <section class="card-info">
                 <div class="card-text">
                     <h3 class="playlist-name">${playlist.playlist_name}</h3>
@@ -40,14 +97,8 @@ async function loadPlaylists() {
                     <button class="like-container" onclick="likePlaylist(event)" data-liked="false"><i id="icon" class="fa-regular fa-heart heart-icon"></i><span class="like-count">${playlist.likes}</span></button>
             </section>
             `;
-            playlistCards.appendChild(playlistCard);
 
-            // add listener for modal to each playlist
-            playlistCard.addEventListener('click', () => {
-                openModal(playlist);
-            })
-        })
-    }
+    return playlistHTML;
 }
 
 function likePlaylist(event) {
@@ -114,7 +165,7 @@ function loadSongs(songs) {
         let songCard = document.createElement('article');
                 songCard.className = 'song-card';
                 songCard.innerHTML = 
-                `
+                    `
                         <div class="flex-container">
                             <img id="song-cover" src=${song.song_cover} alt="Song Cover Image" height="50px;"/>
                             <div>
@@ -126,7 +177,7 @@ function loadSongs(songs) {
                         <div>
                             <p id="timestamp">${song.timestamp}</p>
                         </div>
-                `;
+                    `;
                 songCards.appendChild(songCard);
     });
 }
@@ -150,42 +201,214 @@ async function shuffleSongs(playlist) {
 const featuredCard = document.getElementById('featured-container');
 
 
-async function loadFeatured() {
+async function loadFeatured() {    
     // get playlists and select random index
     const playlists = await fetchData();
     const randomIndex = Math.floor(Math.random() * playlists.length);
     const playlist = playlists[randomIndex];
 
     const playlistCard = document.getElementById('featured-container');
-    //const playlistCard = document.createElement('article');
-    // const playlistCover = document.createElement('img');
-    // playlistCover.className = "playlist-cover";
-    // playlistCover.src = playlist.playlist_art;
 
     // playlistCard.className = 'playlist';
     playlistCard.innerHTML = 
-    `
-    <img class="playlist-cover" src="${playlist.playlist_art}" />
-    <section class="card-text">
-        <h3>${playlist.playlist_name}</h3>
-        <p>${playlist.playlist_author}</p>
-        <button class="like-container" onclick="likePlaylist(event)" data-liked="false"><i id="icon" class="fa-regular fa-heart heart-icon"></i>${playlist.likes}</button>
-    </section>
-    `;
+        `<div id="featured-playlist">
+            <img class="featured-cover" src="${playlist.playlist_art}"></img>
+            <div>
+                <h3 class="playlist-name">${playlist.playlist_name}</h3>
+                <p>${playlist.playlist_author}</p>
+            </div>
+        </div>
+        <div id="featured-songs"></div>
+        `;
 
-    // add listener for modal to each playlist
-    playlistCard.addEventListener('click', () => {
-        openModal(playlist);
-    })
+    playlist.songs.forEach(song => {
+        let featuredSongsCard = document.getElementById('featured-songs');
+        let songCard = document.createElement('article');
+                songCard.className = 'song-card';
+                songCard.innerHTML = 
+                    `
+                        <div class="flex-container">
+                            <img id="song-cover" src=${song.song_cover} alt="Song Cover Image" height="50px;"/>
+                            <div>
+                                <p id="song-title">${song.song_title}</p>
+                                <p id="artist-name">${song.artist_name}</p>
+                                <p id="album-name">${song.album_name}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <p id="timestamp">${song.timestamp}</p>
+                        </div>
+                    `;
+                featuredSongsCard.appendChild(songCard);
+    });
 }
 
 // stretch features
-function editPlaylist(event) {
+async function editPlaylist(event) {
+    // TODO:
+    // open Modal equivalent to AddModal but with default values of the playlist's fields
+    // instead of deleting, modify that playlist in the array since the event.target.id is the playlistID
     event.stopPropagation();
-    console.log('edited');
+    const playlists = await fetchData();
+    // find playlist by id
+    const playlist = playlists.find(playlist => playlist.playlistID === event.target.id);
+
+    openEditModal(playlist);
 }
 
-function deletePlaylist(event) {
+async function deletePlaylist(event) {
     event.stopPropagation();
-    console.log('deleted');
+
+    let playlists = await fetchData();
+    // filter out the playlist with the id that matches
+    playlists = playlists.filter(playlist => playlist.playlistID !== event.target.id);
+    // reload playlists
+    loadPlaylists(playlists);
+}
+
+// Add Playlists
+let numSongs = 0; 
+
+function addSongSection(event) {
+    event.preventDefault();
+    const songSection = document.getElementById('song-section');
+
+    const newSong = document.createElement('section');
+    newSong.className = "song";
+
+    newSong.innerHTML =
+        `
+            <label for="song-name${numSongs}">Song Name: </label>
+            <input type="text" id="song-name${numSongs}" name="song-name${numSongs}">
+        
+            <br />
+        
+            <label for="artist-name${numSongs}">Song Artist: </label>
+            <input type="text" id="artist-name${numSongs}" name="artist-name${numSongs}">
+
+            <br />
+
+            <label for="album-name${numSongs}">Album Name: </label>
+            <input type="text" id="album-name${numSongs}" name="album-name${numSongs}">
+
+            <br />
+
+            <label for="song-length${numSongs}">Song Length: </label>
+            <input type="text" id="song-length${numSongs}" name="song-length${numSongs}">
+
+            <br />
+
+            <label for="song-cover${numSongs}">Song Cover: </label>
+            <input type="file" id="song-cover${numSongs}" name="song-cover${numSongs}" accept="image/png, image/jpeg" />
+        `;
+
+    songSection.insertBefore(newSong, songSection.firstChild);
+    numSongs++;
+}
+
+async function handlePlaylistCreation(event) {
+    event.preventDefault();
+    console.log('playlist created');
+    let playlists = await fetchData();
+
+    // playlist info
+    const playlist_name = document.getElementById('playlist-new-name').value;
+    const playlist_author = document.getElementById('author').value;
+    const playlist_art = "assets/img/playlist.png"; // document.getElementById('cover-image').value;
+    const songs = [];
+
+    // song info, make a song for each song added
+    for (i = 0; i < numSongs; i++) {
+        const song_title = document.getElementById(`song-name${i}`).value;
+        const artist_name = document.getElementById(`artist-name${i}`).value;
+        const album_name = document.getElementById(`album-name${i}`).value;
+        const timestamp = document.getElementById(`song-length${i}`).value;
+
+        //const song_cover = document.getElementById('song-cover').value;
+
+        const newSong = {
+            song_title,
+            artist_name,
+            album_name,
+            timestamp,
+            song_cover: 'assets/img/playlist.png'
+        }
+
+        songs.push(newSong);
+    }
+
+    // make playlist object
+    const newPlaylist = {
+        playlistID: '' + playlists.length,
+        playlist_name,
+        playlist_author,
+        playlist_art,
+        songs,
+        likes: 0
+    }
+
+    numSongs = 0;
+    playlists.unshift(newPlaylist);
+
+    closeAddModal();
+    loadPlaylists(playlists);
+    // const playlistCards = document.getElementById('playlist-cards');
+    // playlistCards.insertBefore(createPlaylist(newPlaylist), playlistCards.firstChild);
+}
+
+// Add Modal
+const addModal = document.getElementById("add-modal");
+function openAddModal() {
+    addModal.style.display = 'block';
+    document.getElementById('add-form').onsubmit = (event) => {
+        handlePlaylistCreation(event);
+    };
+}
+
+function closeAddModal() {
+    const playlist_name = document.getElementById('playlist-new-name');
+    const playlist_author = document.getElementById('author');
+    const playlist_art = document.getElementById('cover-image');
+
+    document.getElementById('current-cover').src = "";
+    playlist_name.value = '';
+    playlist_author.value = '';
+    playlist_art.value = '';
+
+    addModal.style.display = "none";
+    addModal.style.pointerEffects = "none";
+}
+
+const addSpan = document.getElementsByClassName('close')[1];
+
+addSpan.onclick = function() {
+    closeAddModal();
+}
+
+window.onclick = function(event) {
+    if (event.target == addModal) {
+        closeAddModal();
+  }
+}
+
+function openEditModal(playlist) {
+    addModal.style.display = 'block';
+    document.getElementById('add-form').onsubmit = (event) => {
+        editPlaylistSubmission(event);
+    }
+
+    const playlist_name = document.getElementById('playlist-new-name');
+    const playlist_author = document.getElementById('author');
+    const playlist_art = document.getElementById('cover-image');
+
+    document.getElementById('current-cover').src = `${playlist.playlist_art}`
+    playlist_name.value = `${playlist.playlist_name}`;
+    playlist_author.value = `${playlist.playlist_author}`;
+    playlist_art.value = `${playlist.playlist_art}`;
+}
+
+function editPlaylistSubmission(event) {
+    event.preventDefault();
+    console.log('editing');
+    closeAddModal();
 }
